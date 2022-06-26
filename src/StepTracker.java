c9c6009a3a5d651a1a56ba01a1f9ec11349d8f81
gr1spy <george.pozdnyakov@gmail.com>
2022-06-26 10:30:38 +0300

    repair seriesBuf -.-

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -69,6 +69,9 @@ public class StepTracker {
                 }
             }
         }
+        if (series < seriesBuf) {
+            series = seriesBuf;
+        }
         return series;
     }
 
